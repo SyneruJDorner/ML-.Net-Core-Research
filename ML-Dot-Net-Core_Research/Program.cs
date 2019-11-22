81a5468059d178f4df64d144a8abe3015b224c1a
justindd1994 <justindd1994@gmail.com>
2019-11-22 16:57:18 +0200

    Initial push

---
+++ ML-Dot-Net-Core_Research/Program.cs
@@ -0,0 +1,33 @@
+﻿using ML_Dot_Net_Core_ResearchML.Model;
+using System;
+
+namespace ML_Dot_Net_Core_Research
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Add input data
+            var input = new ModelInput();
+            input.SentimentText = "That is rude.";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}\n\n");
+
+
+            input.SentimentText = "Why hello there.";
+
+            // Load model and predict output of sample data
+            result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}\n\n");
+
+
+            input.SentimentText = "Screw you!!!";
+
+            // Load model and predict output of sample data
+            result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}\n\n");
+        }
+    }
+}
